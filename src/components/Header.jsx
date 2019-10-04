2adc61c205fda801c03f8c15534cfae4cf90cd43
Heladito <nicolasestevanprieto@gmail.com>
2019-10-03 21:23:59 -0500

    fix

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () =>
+{
+    <header className="header">
+        <img className="header__img" src="./logoEggSee.png" alt="Logo :)"/>
+        <div className="header__menu">
+            <div className="header__menu--user">
+                <img src="./user.jpg" alt="User img :)"/>
+                <p>Pan77</p>
+            </div>
+            <ul>
+                <li><a href="/">Cuenta</a></li>
+                <li><a href="/">Cerrar sesión</a></li>
+            </ul>
+        </div>
+    </header>
+}
+
+export default Header;
